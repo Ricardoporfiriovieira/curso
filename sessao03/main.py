6ab412831a8af8af9c9a3cfe9a4a92ab44ae6998
Ricardo Porfírio Vieira <ricardoporfiriovieira@gmail.com>
2022-12-08 20:51:43 -0300

    modified sessao03/main

--- sessao03/main.py
+++ sessao03/main.py
@@ -2,4 +2,16 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-cursos = {1: {"titulo": "dale e sua filosofia", "aulas": 300, "horas": 1000}}
+cursos = {
+    1:{
+        "titulo" : "dale e sua filosofia",
+        "aulas" : 300,
+        "horas" : 1000 
+    },
+    2:{
+        "titulo" : "vapo e sua forma de ver o mundo",
+        "aulas" : 200,
+        "horas" : 700
+    },
+    
+}
