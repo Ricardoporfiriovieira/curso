8bd2832be40c6be588881edaeaaf593449b3ddb1
Ricardo Porfírio Vieira <ricardoporfiriovieira@gmail.com>
2022-12-08 23:37:39 -0300

    Create product

--- sessao03/main.py
+++ sessao03/main.py
@@ -1,4 +1,10 @@
 from fastapi import FastAPI
+from fastapi import HTTPException
+from fastapi import status
+
+from typing import List, Optional
+
+from models import Curso
 
 app = FastAPI()
 
@@ -12,6 +18,38 @@ cursos = {
         "titulo" : "vapo e sua forma de ver o mundo",
         "aulas" : 200,
         "horas" : 700
-    },
-    
+    }, 
+
 }
+
+@app.get("/cursos")
+async def get_cursos():
+    return cursos
+
+@app.get("/cursos/{id_curso}")
+async def query_cursos(id_curso: int):
+    try:
+        query_cursos = cursos[id_curso]
+        query_cursos.update({"id":"{id_curso}"})
+
+        return query_cursos
+    except KeyError:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='Curso não encontrado')
+
+
+
+@app.post("/cursos")
+async def update_curso(curso: Curso, status_code=status.HTTP_201_CREATED):
+
+    prox_id: int = len(cursos)+1
+    cursos[prox_id] = curso
+    del cursos[prox_id].id
+    return cursos[prox_id]
+    
+    
+
+if __name__ == '__main__':
+    import uvicorn
+
+    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
+
--- sessao03/models.py
+++ sessao03/models.py
@@ -9,7 +9,4 @@ class Curso(BaseModel):
     aulas: int
     horas: int
 
-if __name__ == '__main__':
-    import uvicorn
 
-    uvicorn.run("main:app", host="127.0.0.1", port=8000, debug=True)
